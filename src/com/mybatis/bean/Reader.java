10e7e4511e4612390c9fb8dea74094c89d482a34
yanglanyue <583968622@qq.com>
2020-04-29 17:51:58 +0800

    first commit

---
+++ src/com/mybatis/bean/Reader.java
@@ -0,0 +1,80 @@
+package com.mybatis.bean;
+
+public class Reader {
+    private Integer id;
+    private String name;
+    private String ename;
+    private String sex;
+    private Integer age;
+    private Integer high;
+    private Double balance;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getHigh() {
+        return high;
+    }
+
+    public void setHigh(Integer high) {
+        this.high = high;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", ename='" + ename + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age=" + age +
+                ", high=" + high +
+                ", balance=" + balance +
+                '}';
+    }
+}
