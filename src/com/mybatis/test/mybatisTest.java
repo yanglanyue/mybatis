412a31e2e8aae33a6a2fbf120a434f54155d9258
yanglanyue <583968622@qq.com>
2020-04-30 15:06:12 +0800

    master

---
+++ src/com/mybatis/test/mybatisTest.java
@@ -0,0 +1,23 @@
+package com.mybatis.test;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class mybatisTest {
+
+    public void test(){
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+    }
+}
