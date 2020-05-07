151c7b57b8816e3804fefab8cf3d1e1afd92e52a
yanglanyue <583968622@qq.com>
2020-05-07 17:53:23 +0800

    2020-5-7-OneMix

--- src/com/mybatis/test/mybatisTest.java
+++ src/com/mybatis/test/mybatisTest.java
@@ -29,14 +29,18 @@ public class mybatisTest {
     * */
     @Test
     public void test() throws IOException {
+        //1
         String resource = "mybatis-config.xml";
         InputStream inputStream = null;
         inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
+        //2
         //从sqlSessionFactory中获取sqlSession实例，能直接执行已经映射的sql语句
         SqlSession openSession = sqlSessionFactory.openSession();
         try {
+//            3.尚硅谷_MyBatis_HelloWorld 16:56
+            //s:传入xml文件中配置好的sql的唯一标识（也就是id），以免发生id重复，最好将namespace连在id前
             Reader reader = openSession.selectOne("com.mybatis.bean.ReaderMapper.selectReader", 1);
             System.out.println(reader);
         }finally {
@@ -52,7 +56,7 @@ public class mybatisTest {
         //获取SqlSession对象
         SqlSession openSession = sqlSessionFactory.openSession();
         try {
-            Reader reader = openSession.selectOne("com.mybatis.bean.ReaderMapper.selectReader", 1);
+            Reader reader = openSession.selectOne("com.mybatis.bean.ReaderMapper.selectReader", 3);
             System.out.println(reader);
         }finally {
             openSession.close();
